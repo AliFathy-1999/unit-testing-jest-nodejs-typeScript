b0948cb0606116609336e875da724ab32bf563d6
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-11-25 15:27:53 +0200

    Add 2 test cases using fake type

--- src/app/doubles/otherUtils.ts
+++ src/app/doubles/otherUtils.ts
@@ -5,10 +5,22 @@ type stringInfo = {
     length: number,
     extraInfo: Object | undefined
 }
+type loggerServiceCallBack = (arg: string) => void;
 
 const calculateComplexity = (stringInfo: stringInfo) => {
     return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
 }
+
+const toUpperCaseWithCb = (arg: string, cb:loggerServiceCallBack) => {
+    if(!arg){
+        cb('Invalid arg');
+        return ;
+    }
+    cb(`called function with ${arg}`)
+    return arg.toUpperCase();
+}
 export {
-    calculateComplexity
+    calculateComplexity,
+    toUpperCaseWithCb,
+    loggerServiceCallBack
 }
--- src/test/doubles/otherUtils.spec.ts
+++ src/test/doubles/otherUtils.spec.ts
@@ -1,16 +1,28 @@
-import { calculateComplexity } from "../../app/doubles/otherUtils"
+import { stringInfo } from "../../app/Utils";
+import { calculateComplexity, toUpperCaseWithCb } from "../../app/doubles/otherUtils"
 
 describe("Other Utils test suites", () => {
+    //Fake Example 
+    it('toUpperCase- calls callback for invalid argument  - fake', () => {
+        const actual = toUpperCaseWithCb('', () => {})
+        expect(actual).toBeUndefined();
+    })
 
-    it('calculates Complexity', () => {
-        const someInfo = {
+    it('toUpperCase- calls callback for valid argument  - fake', () => {
+        const actual = toUpperCaseWithCb('abc', () => {})
+        expect(actual).toBe('ABC')
+    })
+
+    //Stub Example
+    it('calculates Complexity - stub', () => {
+        const someInfo : Partial<stringInfo>= {
             length: 5,
             extraInfo: {
                 field1: 'someInfo',
                 field2: 'someOtherInfo',
             }
         }
-        const actual = calculateComplexity(someInfo as any)
+        const actual = calculateComplexity(someInfo as stringInfo);
         expect(actual).toBe(10);
     })
 })
