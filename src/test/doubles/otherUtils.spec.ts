b9863b5ff0cafdf3fe84105207d94eef2f7109ea
AliFathy-1999 <ali_ahmed_1999@hotmail.com>
2023-11-25 20:11:32 +0200

    Add Manual mocks and mocks test suites

--- src/test/doubles/otherUtils.spec.ts
+++ src/test/doubles/otherUtils.spec.ts
@@ -2,6 +2,60 @@ import { stringInfo } from "../../app/Utils";
 import { calculateComplexity, toUpperCaseWithCb } from "../../app/doubles/otherUtils"
 
 describe("Other Utils test suites", () => {
+
+    //Track callbacks with Mock
+    describe.only('Tracking callback with Jest Mocks', () => {
+
+        const callBackMock = jest.fn();
+        console.log('callBackMock ', callBackMock.mock);
+        
+        // We need to clear the mock calls before each test (Because of mocks tracking)
+        afterEach(()=>{
+            callBackMock.mockClear();
+        })
+        it('calls callback for invalid argument  - Mock - track calls', () => {
+            const actual = toUpperCaseWithCb('', callBackMock)
+            expect(actual).toBeUndefined();
+            expect(callBackMock).toHaveBeenCalledWith('Invalid arg');
+            expect(callBackMock).toHaveBeenCalledTimes(1);
+        })    
+        
+        it('calls callback for valid argument  - Mock - track calls', () => {
+            const actual = toUpperCaseWithCb('abc', callBackMock)
+            expect(actual).toBe('ABC');
+            expect(callBackMock).toHaveBeenCalledWith(`called function with abc`);
+            expect(callBackMock).toHaveBeenCalledTimes(1);
+        })   
+     })
+    //Track callbacks without Mock
+    describe('Tracking callback', () => {
+        let cbArgs = [];
+        let timeCalled = 0;
+
+        function callBackMock(arg:string){
+            cbArgs.push(arg);
+            timeCalled++;
+        }
+
+        afterEach(()=>{
+            // Clearing tracking fields
+            cbArgs = [];
+            timeCalled = 0;
+        })
+        it('calls callback for invalid argument  - Manual Mock - track calls', () => {
+            const actual = toUpperCaseWithCb('', callBackMock)
+            expect(actual).toBeUndefined();
+            expect(cbArgs).toContain('Invalid arg');
+            expect(timeCalled).toBe(1);
+        })    
+        
+        it('calls callback for valid argument  - Manual Mock - track calls', () => {
+            const actual = toUpperCaseWithCb('abc', callBackMock)
+            expect(actual).toBe('ABC');
+            expect(cbArgs).toContain(`called function with abc`);
+            expect(timeCalled).toBe(1);
+        })    
+    })
     //Fake Example 
     it('toUpperCase- calls callback for invalid argument  - fake', () => {
         const actual = toUpperCaseWithCb('', () => {})
